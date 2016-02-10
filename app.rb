5ab95c19640f6af558b50d861db90fd31d8f727e
Vien Van <vienvonvan@gmail.com>
2016-02-10 08:56:51 -0800

    Homework

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 class RestfulControllerApp < Sinatra::Base
+  get '/' do
+    @foods = ["Broccoli", "Carrors", "Sushi"]
+    erb :"index"
+  end
 
+  get '/songs' do
+    @songs = ["Heart Skipped A Beat", "Crystallized", "Stars"]
+    erb :"song"
+  end
 
 
 end
